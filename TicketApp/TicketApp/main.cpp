02381f1fa057ce52e6193429205b319523836e1d
Teea Cranga <crangaana22@stud.ase.ro>
2024-01-04 21:14:02 +0200

    Return of the knight. Phase 2. Made a template for the menu.

--- TicketApp/TicketApp/main.cpp
+++ TicketApp/TicketApp/main.cpp
@@ -13,9 +13,26 @@ int main() {
 	cout << "\t\t\t\t-----!!TICKET APP!!-----\n";
 	cout << endl << "Available options: ";
 	cout << endl << "1.Add event";
-	cout << endl << "2.Add location";
-	cout << endl << "3.Buy ticket";
-	cout << endl << "4.Check validity";
-	cout << endl << "------------------------------------";
+	cout << endl << "2.See locations";
+	cout << endl << "3.Check validity";
+	cout << endl << "------------------------------------\n";
+	int choice, OK = 1;
+	while (OK) {
+		cin >> choice;
+		OK = 0;
+		switch (choice) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		default:
+			cout << "Invalid number. Try again.";
+			OK = 1;
+			break;
+		}
+	}
+
 	return 0;
 }
